68f3f725d9111f62ddebf1c6e86decbb93c1bad6
thekinita <thekinita@mail.ru>
2024-12-17 08:00:18 +0500

    fix: change name of tokenService to TokenService for cronjobing

--- src/cron-jobs/token-cleanup.job.ts
+++ src/cron-jobs/token-cleanup.job.ts
@@ -1,10 +1,8 @@
 import cron from 'node-cron'
 import TokenService from '../services/token.service'
 
-const tokenService = new TokenService()
-
 cron.schedule('0 0 * * *', async () => {
   console.log('Running token cleanup...')
-  await tokenService.clearExpiredTokens()
+  await TokenService.clearExpiredTokens()
   console.log('Token cleanup completed.')
 })
