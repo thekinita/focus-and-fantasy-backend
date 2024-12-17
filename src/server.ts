7eaf710208f1e2e861839c2f23909f61455bcfc0
thekinita <thekinita@mail.ru>
2024-12-17 07:46:37 +0500

    feat: add env config

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import { createServer } from 'http'
 import { Server } from 'socket.io'
 import app from './app'
-import { env } from './env'
+import { env } from './config/env'
 
 const httpServer = createServer(app)
 const PORT = env.PORT
