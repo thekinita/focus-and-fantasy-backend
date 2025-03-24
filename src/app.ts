faa212e78c889b7178451c238865a6e89a9e24b5
thekinita <thekinita@mail.ru>
2025-03-25 03:35:03 +0500

    feat: add endpoints

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,10 @@ import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import { errorHandler } from './middlewares/error-handler'
 import authRouter from './modules/auth/auth.controller'
+import tasksRouter from './modules/tasks/tasks.controller'
+import tagsRouter from './modules/tags/tags.controller'
+import taskListsRouter from './modules/task-lists/lists.controller'
+import columnsOfTaskListRouter from './modules/columns-of-task-list/columns.controller'
 
 dotenv.config()
 
@@ -13,7 +17,12 @@ app.use(cookieParser())
 app.use(cors())
 app.use(express.json())
 
-app.use('/api', authRouter)
+app.use('/api/auth', authRouter)
+app.use('/api/tasks', tasksRouter)
+app.use('/api/tags', tagsRouter)
+app.use('/api/task-lists', taskListsRouter)
+app.use('/api/columns-of-task-list', columnsOfTaskListRouter)
+
 app.use('/public', express.static('public'))
 
 app.use(errorHandler)
